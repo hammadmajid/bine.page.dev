12d53e6cced72cda74d058a3d2e25cddd4160cb1
Hammad Majid <hammadmajid@proton.me>
2024-08-06 20:28:02 +0500

    Create a Link component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let href: string
+	export let fontWeight: string = 'font-medium'
+</script>
+
+<a
+	{href}
+	class={'underline font-inherit underline-offset-4 decoration-dashed hover:text-blue-500 transition-all duration-350' +
+		' ' +
+		fontWeight}><slot /></a
+>
